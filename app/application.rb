0030e48af73eac044e60795620d92105d3decbee
Johnson Joseph <jhonsonj88@gmail.com>
2019-06-02 23:33:46 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -8,7 +8,7 @@ class Application
  		item_name = req.path.split("/items/").last
  		item = @@items.find{|item| item.name == item_name}
 
- 		if item 
+ 		if item
  			resp.write item.price
  		else
  			resp.write "Item not found."
